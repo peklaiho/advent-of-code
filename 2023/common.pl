8cebe4029768044be407c1bc07cf7c4cc62246ff
Pekka Laiho <pekka.i.laiho@gmail.com>
2023-12-23 20:37:09 +0200

    Day 1 Part 1 is working! Also removed the Path::Tiny library, there is no need for it, just use Perl way to read files!

--- 2023/common.pl
+++ 2023/common.pl
@@ -1,27 +1,25 @@
 use strict;
 use warnings;
 
-use Path::Tiny;
-use autodie;
-
-# https://learn.perl.org/examples/read_write_file.html
+# Read all lines from a file and return
+# an array. Remove trailing newline.
 sub readFile {
-    # shift returns the first element from list, and without
-    # arguments that list is $_ that contains the function arguments
-    # individual arguments can be accessed as $_[0], $_[1], and so on
+    # Take first argument
     my $name = shift;
 
-    my $dir = path('.');
-    my $file = $dir->child($name);
-    return $file->lines;
+    open(my $file, '<', $name);
+    my @lines = <$file>;
+    close $file;
+
+    return map { substr($_, 0, -1) } @lines;
 }
 
-# Return the first digit from a string
+# Return the first digit from a string.
 sub firstDigit {
     my $str = shift;
 
     foreach (split('', $str)) {
-        my $v = ord($_);
+        my $v = ord;
         if ($v >= 48 && $v <= 57) {
             return $_;
         }
@@ -30,15 +28,16 @@ sub firstDigit {
     return;
 }
 
+# Return last digit from a string.
 sub lastDigit {
-    return firstDigit reverse $_;
+    return firstDigit scalar reverse;
 }
 
+# Calculate the sum of numbers in an array.
 sub arraySum {
-    my @list = shift
     my $sum = 0;
 
-    foreach (@list) {
+    foreach (@_) {
         $sum += $_;
     }
 
--- 2023/day01.pl
+++ 2023/day01.pl
@@ -4,9 +4,11 @@ use warnings;
 require './common.pl';
 
 # Read data from file
-my @data = readFile('day01-example.txt');
+my @data = readFile('day01-input.txt');
 
 # Find first and last digit of each line
 my @digits = map { firstDigit($_) . lastDigit($_) } @data;
 
-print arraySum(@digits);
+# Print the sum
+my $sum = arraySum(@digits);
+print "Sum: $sum\n";
