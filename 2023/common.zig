64580c2e45f04558e6484a61dcc6ff026fa5ecee
Pekka Laiho <pekka.i.laiho@gmail.com>
2024-08-25 17:37:29 +0300

    Learning Zig, still a horrible mess...

--- 2023/common.zig
+++ 2023/common.zig
@@ -8,33 +8,90 @@ const allocator = gpa.allocator();
 pub fn readInputFile(filename: []const u8) ![]u8 {
     const file = try std.fs.cwd().openFile(filename, .{});
     defer file.close();
-    const filedata = try file.readToEndAlloc(allocator, 1024 * 1024);
-    return filedata;
+    const data = try file.readToEndAlloc(allocator, 1024 * 1024);
+    return data;
 }
 
-/// Read first digit from string and convert it to integer.
-/// Return NoDigitFound error if no digit is found.
-pub fn firstDigit(str: []const u8) !u8 {
-    for (str) |c| {
+const FindDigitResult = {
+    index: usize,
+    value: u8
+};
+
+/// Read first digit ('0', '1', ...) from string and convert it to integer.
+pub fn firstDigit(str: []const u8) ?FindDigitResult {
+    for (str, 0..) |c, i| {
         if (c >= '0' and c <= '9') {
-            return c - 48;
+            return FindDigitResult{
+                .index = i,
+                .value = c - '0',
+            };
         }
     }
 
-    return error.NoDigitFound;
+    return null;
 }
 
-/// Read last digit from string and convert it to integer.
-/// Return NoDigitFound error if no digit is found.
-pub fn lastDigit(str: []const u8) !u8 {
-    var i = str.len - 1;
+/// Read last digit ('0', '1', ...) from string and convert it to integer.
+pub fn lastDigit(str: []const u8) ?FindDigitResult {
+    var i = str.len;
 
-    while (i >= 0) {
-        if (str[i] >= '0' and str[i] <= '9') {
-            return str[i] - 48;
-        }
+    while (i > 0) {
         i -= 1;
+        if (str[i] >= '0' and str[i] <= '9') {
+            return FindDigitResult{
+                .index = i,
+                .value = str[i] - '0',
+            };
+        }
     }
 
-    return error.NoDigitFound;
+    return null;
+}
+
+const digitWords: [9][]const u8 = .{
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine"
+};
+
+/// Same as firstDigit, but include words ("one", "two", ...).
+pub fn firstDigitWord(str: []const u8) ?FindDigitResult {
+    var result = firstDigit(str);
+
+    for (digitWords, 0..) |word, num| {
+        const res = std.mem.indexOf(u8, str, word);
+        if (res) |i| {
+            if (result == null or i < result.?.index)
+            if (index < 0 or i < index) {
+                index = @intCast(i);
+                value = @intCast(num + 1);
+            }
+        }
+    }
+
+    return if (index >= 0) value else null;
+}
+
+/// Same as lastDigit, but include words ("one", "two", ...).
+pub fn lastDigitWord(str: []const u8) ?u8 {
+    var index: i32 = -1;
+    var value: u8 = 0;
+
+    for (digitWords, 0..) |word, num| {
+        const res = std.mem.lastIndexOf(u8, str, word);
+        if (res) |i| {
+            if (index < 0 or i > index) {
+                index = @intCast(i);
+                value = @intCast(num + 1);
+            }
+        }
+    }
+
+    return if (index >= 0) value else null;
 }
--- 2023/day01.zig
+++ 2023/day01.zig
@@ -1,8 +1,17 @@
 const std = @import("std");
 const common = @import("./common.zig");
 
-pub fn main() !void {
-    const data = try common.readInputFile("day01-input.txt");
+fn makeValue(first: ?u8, last: ?u8) !void {
+    if (first) |tens| {
+        if (last) |ones| {
+            return (tens * 10) + ones;
+        }
+    }
+
+    return error.MissingValue;
+}
+
+fn partOne(data: []const u8) !void {
     var lines = std.mem.splitScalar(u8, data, '\n');
 
     var total: u32 = 0;
@@ -13,11 +22,40 @@ pub fn main() !void {
             continue;
         }
 
-        const first = try common.firstDigit(line);
-        const last = try common.lastDigit(line);
+        const first = common.firstDigit(line);
+        const last = common.lastDigit(line);
 
-        total += (first * 10) + last;
+        total += try makeValue(first, last);
     }
 
     std.debug.print("Part 1: {d}\n", .{total});
+}
+
+fn partTwo(data: []const u8) !void {
+    var lines = std.mem.splitScalar(u8, data, '\n');
+
+    // var total: u32 = 0;
+
+    while (lines.next()) |line| {
+        // Skip over empty lines
+        if (line.len == 0) {
+            continue;
+        }
+
+        const first = common.firstDigitWord(line);
+        const last = common.lastDigitWord(line);
+
+        std.debug.print("{s} {d} {d}\n", .{line, first.?, last.?});
+
+        // total += (first * 10);
+    }
+
+    // std.debug.print("Part 2: {d}\n", .{total});
+}
+
+pub fn main() !void {
+    const data = try common.readInputFile("day01-example2.txt");
+
+    // try partOne(data);
+    try partTwo(data);
 }
