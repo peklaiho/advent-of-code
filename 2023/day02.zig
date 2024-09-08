fc43c55d715dfd502082ae7f43c0268b6ea9b8ce
Pekka Laiho <pekka.i.laiho@gmail.com>
2024-09-08 07:48:52 +0300

    Finish Day 2 Part 2!

--- 2023/day02.zig
+++ 2023/day02.zig
@@ -1,13 +1,16 @@
 const std = @import("std");
 const common = @import("./common.zig");
 
-var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-const allocator = gpa.allocator();
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
 
-fn partOne(data: []const u8) !void {
+    const data = try common.readInputFile("day02-input.txt", allocator);
+
     var lines = std.mem.splitScalar(u8, data, '\n');
 
-    var total: u32 = 0;
+    var total1: u32 = 0;
+    var total2: u32 = 0;
 
     while (lines.next()) |line| {
         // Skip over empty lines
@@ -20,13 +23,13 @@ fn partOne(data: []const u8) !void {
         const colon = std.mem.indexOf(u8, line, ":").?;
         const game_num = try std.fmt.parseInt(u8, line[5 .. colon], 10);
 
-        // std.debug.print("Game {d}\n", .{ game_num });
-
         var sets = std.mem.splitScalar(u8, line[(colon + 1) ..], ';');
 
-        while (sets.next()) |set| {
-            // std.debug.print("Set {s}\n", .{ set });
+        var max_red: u32 = 0;
+        var max_green: u32 = 0;
+        var max_blue: u32 = 0;
 
+        while (sets.next()) |set| {
             var colors = std.mem.splitScalar(u8, set, ',');
 
             while (colors.next()) |num_and_color| {
@@ -34,30 +37,40 @@ fn partOne(data: []const u8) !void {
                 const num = try std.fmt.parseInt(u8, num_and_color[1 .. space], 10);
                 const color = num_and_color[(space + 1) ..];
 
-                if ((num > 12 and std.mem.eql(u8, color, "red")) or
-                        (num > 13 and std.mem.eql(u8, color, "green")) or
-                        (num > 14 and std.mem.eql(u8, color, "blue"))) {
-                    is_valid = false;
-                    break;
+                if (std.mem.eql(u8, color, "red")) {
+                    if (num > 12) {
+                        is_valid = false;
+                    }
+                    if (num > max_red) {
+                        max_red = num;
+                    }
+                } else if (std.mem.eql(u8, color, "green")) {
+                    if (num > 13) {
+                        is_valid = false;
+                    }
+                    if (num > max_green) {
+                        max_green = num;
+                    }
+                } else if (std.mem.eql(u8, color, "blue")) {
+                    if (num > 14) {
+                        is_valid = false;
+                    }
+                    if (num > max_blue) {
+                        max_blue = num;
+                    }
+                } else {
+                    // Unknown color, should not happen
                 }
-            }
-
-            if (!is_valid) {
-                break;
             }
         }
 
         if (is_valid) {
-            total += game_num;
+            total1 += game_num;
         }
+
+        total2 += max_red * max_green * max_blue;
     }
 
-    std.debug.print("Part 1: {d}\n", .{total});
-}
-
-pub fn main() !void {
-    const data = try common.readInputFile("day02-input.txt", allocator);
-
-    try partOne(data);
-    // try partTwo(data);
+    std.debug.print("Part 1: {d}\n", .{total1});
+    std.debug.print("Part 2: {d}\n", .{total2});
 }
