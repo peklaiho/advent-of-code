e91bbecc70c7bb3c6500adc4bc788992f923a8ec
Pekka Laiho <pekka.i.laiho@gmail.com>
2025-03-13 10:01:46 +0200

    Advent of Code 2024 in Assembly: some library functions and tests for them

---
+++ 2024/lib.asm
@@ -0,0 +1,186 @@
+; File descriptors
+STDIN equ 0
+STDOUT equ 1
+STDERR equ 2
+
+; Syscall numbers
+SYS_READ equ 0
+SYS_WRITE equ 1
+SYS_OPEN equ 2
+SYS_CLOSE equ 3
+SYS_EXIT equ 60
+
+; Misc
+NEWLINE equ 10
+
+section .text
+
+;; Exported functions
+global exit, memcmp, memcpy, memset, itos, stoi, read_line, read_file
+
+;; Exit the program
+;; Inputs: RDI = exit code
+
+exit:
+    mov rax, SYS_EXIT
+    syscall                     ; rdi is passed on unchanged
+    ret
+
+;; Compare two memory locations
+;; Inputs: RDI, RSI, RDX = length
+;; Output: RAX = 0, 1, -1
+
+memcmp:
+    mov rcx, rdx
+    cld
+    repe cmpsb                  ; repeat until equal or rcx=0
+    jz .equal
+    jns .negres
+    mov rax, 1
+    ret
+.negres:
+    mov rax, -1
+    ret
+.equal:
+    mov rax, 0
+    ret
+
+;; Copy bytes from source to destination
+;; Inputs: RDI = destination, RSI = source, RDX = length
+
+memcpy:
+    mov rcx, rdx
+    cld
+    rep movsb                   ; copy RCX bytes from RSI to RDI
+    ret
+
+;; Set bytes to specified value
+;; Inputs: RDI = destination, RSI = value to set, RDX = length
+
+memset:
+    mov rax, rsi
+    mov rcx, rdx
+    cld
+    rep stosb
+    ret
+
+;; Convert integer to null-terminated string
+;; Inputs: RDI = string, RSI = integer
+
+itos:
+    cld
+    xor rcx, rcx
+    mov rax, rsi
+    mov rsi, 10                 ; divisor
+    xor r8, r8                  ; r8 marks negative
+    cmp rax, 0
+    jge .loopPush               ; jump if value >= 0
+    mov r8, 1
+    neg rax
+.loopPush:
+    mov rdx, 0                  ; rdx is used by div so make sure it is zero
+    div rsi                     ; divide by 10, remainder is stored in rdx
+    add rdx, '0'
+    push rdx                    ; push the digit to stack
+    inc rcx
+    test rax, rax
+    jnz .loopPush               ; continue if we have more digits
+    test r8, r8
+    jz .loopPop
+    mov rax, '-'
+    stosb
+.loopPop:
+    pop rax
+    stosb
+    dec rcx
+    test rcx, rcx
+    jnz .loopPop                ; continue if we have more digits
+    mov byte [rdi], 0           ; null-terminator
+    ret
+
+;; Convert string to integer
+;; Inputs: RDI
+;; Output: RAX
+
+stoi:
+    xor rax, rax
+    xor rcx, rcx
+    mov rsi, 10                 ; multiplier
+    xor r8, r8                  ; r8 marks negative
+    mov cl, [rdi]
+    cmp cl, '-'
+    jne .loop
+    mov r8, 1
+    inc rdi
+.loop:
+    mov cl, [rdi]
+    cmp cl, '0'
+    jb .finish                  ; jump to exit if char is < '0'
+    cmp cl, '9'
+    ja .finish                  ; jump to exit if char is > '9'
+    mul rsi                     ; multiply previous value
+    sub cl, '0'
+    add rax, rcx                ; add value to rax
+    inc rdi
+    jmp .loop                   ; start over
+.finish:
+    test r8, r8
+    jz .finish2
+    neg rax                     ; make negative
+.finish2:
+    ret
+
+;; Read line to buffer (until newline or null-terminator)
+;; The newline character itself is not included
+;; Inputs: RDI = destination, RSI = source
+;; Output: RAX = number of chars read (including newline)
+
+read_line:
+    cld
+    xor rcx, rcx
+.loop:
+    lodsb                       ; read char
+    cmp al, 0
+    je .finish                  ; finish if null-terminator
+    cmp al, NEWLINE
+    je .finish_nl               ; finish if \n
+    stosb                       ; write char
+    inc rcx
+    jmp .loop
+.finish_nl:
+    inc rcx                     ; include newline char in count
+.finish:
+    mov byte [rdi], 0           ; null-terminator
+    mov rax, rcx
+    ret
+
+;; Read file contents into memory
+;; Inputs: RDI = buffer, RSI = filename
+;; Output: RAX = number of bytes read
+
+read_file:
+    push rdi                    ; store buf
+
+    ; open file
+    mov rdi, rsi                ; arg1: filename
+    mov rsi, 0                  ; arg2: flags, 0 = O_RDONLY
+    mov rdx, 0                  ; arg3: mode
+    mov rax, SYS_OPEN
+    syscall
+
+    ; read file
+    mov rdi, rax                ; arg1: file-pointer
+    pop rsi                     ; arg2: buffer
+    mov rdx, 1048576            ; arg3: length, 1mb
+    mov rax, SYS_READ
+    push rdi                    ; store file-pointer
+    syscall
+
+    ; close file
+    pop rdi                     ; arg1: file-pointer
+    push rax                    ; store bytes read
+    mov rax, SYS_CLOSE
+    syscall
+
+    pop rax                     ; return bytes read
+    ret
---
+++ 2024/test.asm
@@ -0,0 +1,136 @@
+%include "macro.asm"
+
+extern exit, memcmp, memcpy, memset, itos, stoi, read_line
+
+section .data
+    lines db `opq\nrst\n\0`
+    num1 db "12345", 0
+    num2 db "-98765", 0
+    word1 db "abc", 0
+    word2 db "acd", 0
+    empty db 0
+
+section .bss
+    buf resb 256
+
+section .text
+
+global _start
+_start:
+    call test_memcmp
+    call test_memcpy
+    call test_memset
+    call test_itos
+    call test_stoi
+    call test_read_line
+    call1 exit, 0
+
+test_memcmp:
+    call3 memcmp, word1, word2, 3
+    cmp rax, -1
+    jne .error
+    call3 memcmp, word2, word1, 3
+    cmp rax, 1
+    jne .error
+    call3 memcmp, word1, word1, 3
+    cmp rax, 0
+    jne .error
+    jmp .finish
+.error:
+    call1 exit, 1
+.finish:
+    ret
+
+test_memcpy:
+    call3 memcpy, buf, word2, 3
+    mov al, [buf]
+    cmp al, 'a'
+    jne .error
+    mov al, [buf + 1]
+    cmp al, 'c'
+    jne .error
+    mov al, [buf + 2]
+    cmp al, 'd'
+    jne .error
+    jmp .finish
+.error:
+    call1 exit, 2
+.finish:
+    ret
+
+test_memset:
+    call3 memset, buf, 5, 3
+    mov al, [buf]
+    cmp al, 5
+    jne .error
+    mov al, [buf + 1]
+    cmp al, 5
+    jne .error
+    mov al, [buf + 2]
+    cmp al, 5
+    jne .error
+    jmp .finish
+.error:
+    call1 exit, 3
+.finish:
+    ret
+
+test_itos:
+    call2 itos, buf, 45
+    mov al, [buf]
+    cmp al, '4'
+    jne .error
+    mov al, [buf + 1]
+    cmp al, '5'
+    jne .error
+    mov al, [buf + 2]
+    cmp al, 0               ; check null-terminator
+    jne .error
+    jmp .finish
+.error:
+    call1 exit, 4
+.finish:
+    ret
+
+test_stoi:
+    call1 stoi, num1
+    cmp rax, 12345
+    jne .error
+    call1 stoi, num2
+    cmp rax, -98765
+    jne .error
+    jmp .finish
+.error:
+    call1 exit, 5
+.finish:
+    ret
+
+test_read_line:
+    ; testcase 1
+    call2 read_line, buf, lines
+    cmp rax, 4              ; check length
+    jne .error
+    mov al, [buf]
+    cmp al, 'o'
+    jne .error
+    mov al, [buf + 1]
+    cmp al, 'p'
+    jne .error
+    mov al, [buf + 2]
+    cmp al, 'q'
+    jne .error
+    mov al, [buf + 3]
+    cmp al, 0
+    jne .error
+    ; testcase 2: empty string
+    call2 read_line, buf, empty
+    cmp rax, 0
+    jne .error
+    mov al, [buf]
+    cmp al, 0
+    jne .error
+    jmp .finish
+.error:
+    call1 exit, 6
+.finish:
+    ret
