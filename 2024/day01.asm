06e19b3d25b7f0acb9738efa08e2b407447c32ec
Pekka Laiho <pekka.i.laiho@gmail.com>
2025-03-14 16:39:09 +0200

    Day 01 in progress: reading integers to two arrays is working

--- 2024/day01.asm
+++ 2024/day01.asm
@@ -1,6 +1,6 @@
 %include "macro.asm"
 
-extern exit, prints, memset, stoi, read_line, read_file
+extern exit, prints, read_two_ints, read_line, read_file
 
 section .data
     filename db "day01-sample.txt", 0
@@ -16,38 +16,46 @@ section .text
 
 global _start
 _start:
+    mov r12, raw_data           ; r12 is raw data buffer
+
     ; read file into memory
-    call3 read_file, raw_data, filename, 32768
-    mov r14, raw_data
-    add r14, rax                ; store end in r14
+    call3 read_file, r12, filename, 32768
+    mov r13, r12
+    add r13, rax                ; r13 is end of raw data
 
     ; add null-terminator
-    mov byte [r14], 0
+    mov byte [r13], 0
 
-    ; parse integers
-    mov r12, raw_data           ; r12 is byte index
-    xor r13, r13                ; r13 is line index
+    mov r14, col1               ; r14 is index of col1
+    mov r15, col2               ; r15 is index of col2
 
-.parseLoop:
-    ; read next line to buf
+.parse:
+    ; read next line into buf
     call2 read_line, buf, r12
-    add r12, rax                 ; increment r12 by bytes read
-    inc r12                      ; +1 to include newline char
+    add r12, rax                ; increment r12 by bytes read
+    inc r12                     ; +1 to include newline char
 
-    ; read number
-    call1 stoi, buf
-.readNum:
-    mov dword [col1 + r13], eax
-    inc r13
+    ; read numbers into col1, col2
+    call1 read_two_ints, buf
+    mov dword [r14], eax        ; write first number to col1
+    shr rax, 32                 ; shift right
+    mov dword [r15], eax        ; write second number to col2
 
-    ; check stop condition
-    cmp r12, r14
-    jge .finish
+    ; increment registers
+    add r14, 4
+    add r15, 4
 
-    ; back to start of loop
-    jmp .parseLoop
+    cmp r12, r13
+    jl .parse                   ; continue if r12 < r13
+
+    ; qty of numbers: (r14 - col1) / 4
+    ; save it to col_len
+    mov rax, r14
+    sub rax, col1
+    xor rdx, rdx
+    mov rdi, 4
+    div rdi
+    mov dword [col_len], eax
 
 .finish:
-
-    ; finish
     call1 exit, 0
