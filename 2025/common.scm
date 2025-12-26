fde6a5f8e0aa92b48e6b49e836a5f337bff80b1c
Pekka Laiho <pekka.i.laiho@gmail.com>
2025-12-26 13:55:38 +0200

    Finish Day 4 Part 1!

--- 2025/common.scm
+++ 2025/common.scm
@@ -14,9 +14,35 @@
       (lambda (port)
         (reverse (read-lines-from-port port '()))))))
 
+;; (number-list-to-number '(1 2 3 4)) -> 1234
 (define number-list-to-number
   (lambda (lst)
     (string->number
      (list->string
       (map (lambda (num)
              (integer->char (+ num (char->integer #\0)))) lst)))))
+
+(define make-number-list-helper
+  (lambda (lst num)
+    (cond
+     [(= (length lst) num) lst]
+     [else (make-number-list-helper (cons (length lst) lst) num)])))
+
+;; Make list of numbers from 0 to len-1
+;; (make-number-list 5) -> (0 1 2 3 4)
+(define make-number-list
+  (lambda (len)
+    (reverse (make-number-list-helper '() len))))
+
+;; Make list of all possible coordinate pairs for a grid
+;; (make-coordinate-list 3 2) ->
+;; ((0 0) (0 1)
+;;  (1 0) (1 1)
+;;  (2 0) (2 1))
+(define make-coordinate-list
+  (lambda (rows cols)
+    (apply append
+           (let ([row-range (make-number-list rows)]
+                 [col-range (make-number-list cols)])
+             (map (lambda (y)
+                    (map (lambda (x) (list y x)) col-range)) row-range)))))
--- 2025/day04.scm
+++ 2025/day04.scm
@@ -1,11 +1,7 @@
 (load "./common.scm")
 
 ;; Return the value from grid by row/col
-(define grid-ref
-  (lambda (grid row col)
-    (list-ref (list-ref grid row) col)))
-
-;; Same as grid-ref, but return zero if out of bounds
+;; Return zero if out of bounds
 (define grid-ref-check-bounds
   (lambda (grid row col)
     (cond
@@ -29,6 +25,26 @@
      (grid-ref-check-bounds grid (+ y 1) x)
      (grid-ref-check-bounds grid (+ y 1) (+ x 1)))))
 
+;; Return 1 for valid cell, otherwise 0
+(define is-valid-cell
+  (lambda (grid y x)
+    (if (and (= (grid-ref-check-bounds grid y x) 1)
+             (< (count-neighbors grid y x) 4)) 1 0)))
+
+(define process-grid
+  (lambda (grid)
+    (let* ([rows (length grid)]
+           [cols (length (car grid))]
+           [coords (make-coordinate-list rows cols)])
+      (map (lambda (yx)
+             (let* ([y (car yx)]
+                    [x (cadr yx)]
+                    [result (is-valid-cell grid y x)])
+               ;; (write-line (string-append "Result for " (number->string y) ", "
+               ;;                            (number->string x) ": " (number->string result)))
+               result))
+             coords))))
+
 ;; Lets map @ to 1 and . to 0, because we like working with ones and zeroes
 (define prepare-grid
   (lambda (raw-input)
@@ -37,5 +53,6 @@
                   (if (char=? ch #\.) 0 1))
                 (string->list row))) raw-input)))
 
-(let ([grid (prepare-grid (read-file-lines "day04-sample.txt"))])
-  (count-neighbors grid 0 2))
+(let* ([grid (prepare-grid (read-file-lines "day04-input.txt"))]
+       [result (reduce + 0 (process-grid grid))])
+  (write-line (string-append "Result for Part 1: " (number->string result))))
